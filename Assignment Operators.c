49b71bab436183fb92767adcab42ef7a45ad2b52
poojithagottipati <160849305+poojithagottipati@users.noreply.github.com>
2024-03-14 10:32:17 +0530

    Create Assignment Operators.c

---
+++ Assignment Operators.c
@@ -0,0 +1,46 @@
+1. Addition Assignment
+  #include <stdio.h>
+
+int main() {
+    int x = 5;
+    x += 3;  // Equivalent to: x = x + 3;
+    printf("Value of x: %d\n", x);  // Output: Value of x: 8
+    return 0;
+}
+2. Subtraction Assignment
+#include <stdio.h>
+
+int main() {
+    int x = 8;
+    x -= 3;  // Equivalent to: x = x - 3;
+    printf("Value of x: %d\n", x);  // Output: Value of x: 5
+    return 0;
+}
+3. Multiplication assignment
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    x *= 2;  // Equivalent to: x = x * 2;
+    printf("Value of x: %d\n", x);  // Output: Value of x: 10
+    return 0;
+}
+
+4. Division Assignment
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    x /= 2;  // Equivalent to: x = x / 2;
+    printf("Value of x: %d\n", x);  // Output: Value of x: 5
+    return 0;
+}
+5. Modulus Assignment
+#include <stdio.h>
+
+int main() {
+    int x = 10;
+    x %= 3;  // Equivalent to: x = x % 3;
+    printf("Value of x: %d\n", x);  // Output: Value of x: 1
+    return 0;
+}
