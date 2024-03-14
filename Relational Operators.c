51f5f0cdaa85e1b3cc752a061ff603db019348ab
poojithagottipati <160849305+poojithagottipati@users.noreply.github.com>
2024-03-14 10:28:04 +0530

    Create Relational Operators.c

---
+++ Relational Operators.c
@@ -0,0 +1,62 @@
+1. =
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 5;
+    if (a == b) {
+        printf("a is equal to b\n");  // Output: a is equal to b
+    }
+    return 0;
+}
+2. !=
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 7;
+    if (a != b) {
+        printf("a is not equal to b\n");  // Output: a is not equal to b
+    }
+    return 0;
+}
+3. >
+#include <stdio.h>
+
+int main() {
+    int a = 8, b = 5;
+    if (a > b) {
+        printf("a is greater than b\n");  // Output: a is greater than b
+    }
+    return 0;
+}
+4. <
+  #include <stdio.h>
+
+int main() {
+    int a = 3, b = 6;
+    if (a < b) {
+        printf("a is less than b\n");  // Output: a is less than b
+    }
+    return 0;
+}
+5. >=
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 5;
+    if (a >= b) {
+        printf("a is greater than or equal to b\n");  // Output: a is greater than or equal to b
+    }
+    return 0;
+}
+6. <=
+#include <stdio.h>
+
+int main() {
+    int a = 4, b = 7;
+    if (a <= b) {
+        printf("a is less than or equal to b\n");  // Output: a is less than or equal to b
+    }
+    return 0;
+}
+
+  
